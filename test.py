84fb9787fb558699e41bd544b4a9b3ee6322f6b6
Duncan Miller <dmiller@heatspring.com>
2023-12-20 10:09:43 -0800

    add a test for the a response from openai api

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+load_dotenv()
+
+class TestOpenAIResponse(unittest.TestCase):
+    def test_aip_connection(self):
+
+        prompt = "hello"
+
+        response = client.completions.create(model="text-davinci-003",
+                                             prompt=prompt,
+                                             max_tokens=100)
+
+        self.assertTrue(response)
+
+if __name__ == '__main__':
+    unittest.main()
