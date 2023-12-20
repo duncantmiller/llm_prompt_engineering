bd5cca6e38b7d7969a9b1a1152e808146822ad85
Duncan Miller <dmiller@heatspring.com>
2023-12-20 10:34:23 -0800

    refactored message class and passing test

--- message.py
+++ message.py
@@ -1,12 +1,13 @@
 from client import Client
 
 class Message():
-    def __init__(self):
+    def __init__(self, prompt):
         client = Client()
         self.client = client.openai_client
+        self.prompt = prompt
 
-    def ask_client(self, prompt):
+    def ask_client(self):
         response = self.client.completions.create(model="text-davinci-003",
-                                                prompt=prompt,
+                                                prompt=self.prompt,
                                                 max_tokens=100)
         return response
