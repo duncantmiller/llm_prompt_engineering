cdca5fc794df615311dc1c0159a4124bfdda890f
Duncan Miller <dmiller@heatspring.com>
2023-12-20 12:05:33 -0800

    refactor tests to move duplicate variables into setup

--- test.py
+++ test.py
@@ -12,70 +12,57 @@ class TestClient(unittest.TestCase):
         self.assertTrue(response)
 
 class TestMessage(unittest.TestCase):
+    def setUp(self):
+        self.prompt = "hello"
+        self.message = Message(self.prompt)
+
     def test_ask_client(self):
-        message = Message("hello")
-        response = message.ask_client()
+        response = self.message.ask_client()
 
         self.assertTrue(response)
 
     def test_full_prompt(self):
-        prompt = "hello"
-        message = Message(prompt)
-        full_prompt = message.full_prompt()
-
-        self.assertEqual(full_prompt, f"{prompt} {message.cite_sources_prompt()}")
-
-    def test_full_prompt(self):
-        prompt = "hello"
-        message = Message(prompt)
-        full_prompt = message.full_prompt()
+        full_prompt = self.message.full_prompt()
 
         self.assertEqual(
-            full_prompt, f"{message.pre_prompt()} {prompt} {message.cite_sources_prompt()}"
+            full_prompt,
+            f"{self.message.pre_prompt()} {self.prompt} {self.message.cite_sources_prompt()}"
         )
 
-    def test_response_includes_citation(self):
+class TestMessageResponse(unittest.TestCase):
+    def setUp(self):
         sample_prompt = "Explain the theory of relativity"
         message = Message(sample_prompt)
         response = message.ask_client()
-        response_text = response.choices[0].text
+        self.response_text = response.choices[0].text
 
+    def test_response_includes_citation(self):
         self.assertIn(
-            "citation", response_text.lower(), "Response should include the <citations:> tag"
-        )
-
-    def test_default_does_not_include_citation(self):
-        sample_prompt = "Explain the theory of relativity"
-        client = Client().openai_client
-        response = client.completions.create(model="text-davinci-003",
-                                             prompt=sample_prompt,
-                                             max_tokens=1000)
-        response_text = response.choices[0].text
-
-        self.assertNotIn(
-            "citation", response_text.lower(), "Response should not include the <citations:> tag"
+            "citation", self.response_text.lower(), "Response should include the <citations:> tag"
         )
 
     def test_response_includes_pre_prompt(self):
-        sample_prompt = "Explain the theory of relativity"
-        message = Message(sample_prompt)
-        response = message.ask_client()
-        response_text = response.choices[0].text
-
         self.assertIn("my dedicated student",
-                      response_text.lower(),
+                      self.response_text.lower(),
                       "Response should comply with pre_prompt instructions")
 
-    def test_default_does_not_include_pre_prompt(self):
+class TestDefaultResponse(unittest.TestCase):
+    def setUp(self):
         sample_prompt = "Explain the theory of relativity"
         client = Client().openai_client
         response = client.completions.create(model="text-davinci-003",
                                              prompt=sample_prompt,
                                              max_tokens=1000)
-        response_text = response.choices[0].text
+        self.response_text = response.choices[0].text
 
+    def test_does_not_include_citation(self):
+        self.assertNotIn(
+            "citation", self.response_text.lower(), "Response should not include the <citations:> tag"
+        )
+
+    def test_does_not_include_pre_prompt(self):
         self.assertNotIn("my dedicated student",
-                response_text.lower(),
+                self.response_text.lower(),
                 "Response should not include pre_prompt instructions")
 
 if __name__ == '__main__':
