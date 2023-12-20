752096db746600e271832cbff2c1e42accd39238
Duncan Miller <dmiller@heatspring.com>
2023-12-20 10:15:53 -0800

    failing test for ask_client method

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from main import *
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -17,6 +18,13 @@ class TestOpenAIResponse(unittest.TestCase):
                                              max_tokens=100)
 
         self.assertTrue(response)
+
+class TestMain(unittest.TestCase):
+    def test_ask_client(self):
+        response = ask_client("hello")
+
+        self.assertTrue(response)
+
 
 if __name__ == '__main__':
     unittest.main()
