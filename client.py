0f20690d7820d4026cf03ca463ac36be1f481ee4
Duncan Miller <dmiller@heatspring.com>
2023-12-25 18:07:36 -0800

    add constant for gpt-4

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Client():
     MODEL_TEXT_DAVINCI = "text-davinci-003"
     MODEL_GPT_35 = "gpt-3.5-turbo"
+    MODEL_GPT_4 = "gpt-4-1106-preview"
 
     def __init__(self):
         self.openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
