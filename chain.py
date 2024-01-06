58a80688ba0d52031571e8ea764322a2bb6ab5df
Duncan Miller <dmiller@heatspring.com>
2024-01-06 15:29:16 -0800

    add webbaseloader to pull content from a url

--- chain.py
+++ chain.py
@@ -1,17 +1,20 @@
 from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import WebBaseLoader
 
 llm = ChatOpenAI()
-
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a world class professor"),
     ("user", "{input}")
 ])
-
 output_parser = StrOutputParser()
 
-chain = prompt | llm | output_parser
+loader = WebBaseLoader("https://botdevs.ai/articles/prompt-engineering-testing-strategies-with-python")
+docs = loader.load()
 
-response = chain.invoke({"input": "hello?"})
-print(response)
+# chain = prompt | llm | output_parser
+
+# response = chain.invoke({"input": "hello?"})
+# print(response)
+print(docs)
